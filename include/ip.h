3dba0ca970250ad7aae2fe3fd06099d018db792d
eshenhu <eshenhu@gmail.com>
2018-05-30 16:26:52 +0000

    add example and header files

---
+++ include/ip.h
@@ -0,0 +1,11 @@
+#ifndef _IP_H_H_
+#define _IP_H_H_
+
+/*
+ *  This function was used as the main entry to the
+ *  iproute2 functionality.
+ *
+ */
+int main_entry_point(int argc, char **argv);
+
+#endif
